587be60787c66101f52332cc9b92b3889839fc0b
Xavier <143090232+xavier-kirkpatrick@users.noreply.github.com>
2023-10-12 15:25:26 +1300

    scoreFrames function passing

---
+++ score.js
@@ -0,0 +1,5 @@
+export { scoreFrame }
+
+function scoreFrame(frame) {
+  return 0
+}
--- score.test.js
+++ score.test.js
@@ -1,5 +1,14 @@
 import { expect, test } from 'vitest'
+import * as score from './score'
 
 test('Try out first test, which always pass', () => {
   expect(true).toBeTruthy
+})
+
+test('scores a gutterball frame', () => {
+  const frame = [0, 0]
+  const expected = 0
+  const actual = score.scoreFrame(frame)
+
+  expect(actual).toBe(expected)
 })
